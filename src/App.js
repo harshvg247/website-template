28261c071121d12900d7938a718ac054fd7cbddf
harshvg247 <harsh247gupta@gmail.com>
2023-08-24 18:04:23 +0530

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Section1 from "./Section1";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Section4 from "./Section4";
+import Section11 from "./Section11";
 import { gsap } from "gsap";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section11/>
     </div>
   );
 }
