3454bf11a094113275ffc904ace6fc8058f6e94c
Kaustubh Rakhade <kaustubhrakhade@gmail.com>
2023-08-24 18:51:51 +0530

    add timeline section

--- src/Section5.js
+++ src/Section5.js
@@ -1,1 +1,45 @@
-// timeline
+import React from 'react'
+import "./styles/timeline.css"
+
+export default function Section5() {
+  return (
+    <div id='timeline-section'>
+        <h5>This is a subtitle</h5>
+        <h2>The Timeline Title</h2>
+        <div id="timeline">
+        <div id="timeline-content">
+    
+    <div className="timeline-item">
+        <h6>This is an event</h6>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit voluptate ratione iusto consequuntur blanditiis nostrum rem architecto delectus.</span>
+    </div>
+    <div className="timeline-item">
+        <h6>This is an event</h6>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit voluptate ratione iusto consequuntur blanditiis nostrum rem architecto delectus.</span>
+    </div>
+    <div className="timeline-item">
+        <h6>This is an event</h6>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit voluptate ratione iusto consequuntur blanditiis nostrum rem architecto delectus.</span>
+    </div>
+    <div className="timeline-item">
+        <h6>This is an event</h6>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit voluptate ratione iusto consequuntur blanditiis nostrum rem architecto delectus.</span>
+    </div>
+    <div className="timeline-item">
+        <h6>This is an event</h6>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit voluptate ratione iusto consequuntur blanditiis nostrum rem architecto delectus.</span>
+    </div>
+    <div className="timeline-item">
+        <h6>This is an event</h6>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit voluptate ratione iusto consequuntur blanditiis nostrum rem architecto delectus.</span>
+    </div>
+    <div className="timeline-item">
+        <h6>This is an event</h6>
+        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab suscipit voluptate ratione iusto consequuntur blanditiis nostrum rem architecto delectus.</span>
+    </div>
+</div>   
+        </div>
+
+    </div>
+  )
+}
